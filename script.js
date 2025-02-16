04b7f52b3e0c44612875a3953a4a91a8ed2f137c
Optional-Josh <caguimbaljoshu21@gmail.com>
2025-02-16 08:13:50 +0800

    create and edit elements

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ displayButton.addEventListener("click", sentToDisplay);
 function sentToDisplay() {
   display.textContent = entry.value;
 }
+
+let expenseBtn = document.querySelector("#expenseBtn");
+let incomeBtn = document.querySelector("#incomeBtn");
+
+let expenseDiv = document.querySelector(".expenses");
+let incomeDiv = document.querySelector(".income");
+
+expenseBtn.addEventListener("click", addExpense);
+
+function addExpense() {
+  let newDiv = document.createElement("div");
+  let newContent = document.createTextNode("Hi and this is an expense");
+
+  newDiv.appendChild(newContent);
+  expenseDiv.appendChild(newDiv);
+}
